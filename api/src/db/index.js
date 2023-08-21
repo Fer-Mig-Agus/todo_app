0b65d91ca7df19ff2fb25a270dd9c1db86f1d403
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2023-08-21 14:19:16 -0300

    he agregado el db_web

--- api/src/db/index.js
+++ api/src/db/index.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const { DEBUG, DB_USER, DB_PASSWORD, DB_DIALECT, DB_PORT, DB_HOST, DB_NAME } = process.env;
+const { DEBUG, DB_USER, DB_PASSWORD, DB_DIALECT, DB_PORT, DB_HOST, DB_NAME, DB_WEB } = process.env;
 
-if(DEBUG === "true"){
+if (DEBUG === "true") {
     const baseDeDatos = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
         port: DB_PORT,
         dialect: DB_DIALECT,
@@ -12,7 +12,7 @@ if(DEBUG === "true"){
     })
 
     module.exports = baseDeDatos;
-}else{
+} else {
     //Render deploy database
     const baseDeDatos = new Sequelize(DB_WEB, {
         logging: false, // set to console.log to see the raw SQL queries
